045939a2add61f6fb2376016a4962848058a9cb7
helloween <yde773786@gmail.com>
2022-08-08 00:42:21 -0400

    Register functional

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const {getDb} = require("../database/database");
 var router = express.Router();
 
 
@@ -6,5 +7,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('register');
 });
+
+router.post('/', function (req, res, next) {
+
+    let db = getDb();
+
+    let dbo = db.db("crud");
+    dbo.collection('Users').insertOne({
+        "username": req.body.username,
+        "password": req.body.password,
+        "email": req.body.email,
+        "name": req.body.name
+        }).then((value) => {
+            console.log(value);
+        }
+    ).catch((err) => {
+        console.log('Resource unavailable: ', err);
+    })
+})
 
 module.exports = router;
