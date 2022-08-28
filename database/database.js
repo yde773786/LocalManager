2cc89d3eec1232893121ab2c3f1eb655a23fd72b
helloween <yde773786@gmail.com>
2022-08-28 02:46:14 -0400

    wait for connect before connecting

--- database/database.js
+++ database/database.js
@@ -2,21 +2,24 @@ const { MongoClient, ServerApiVersion} = require("mongodb");
 
 let mongoose;
 
-const url = "mongodb+srv://crud:y7TAts5GZb63Szq@crud.lslvjjf.mongodb.net/?retryWrites=true&w=majority";
+const url = 'mongodb+srv://crud:zjz1lNPmAkEWEKI3@crud.lslvjjf.mongodb.net/?retryWrites=true&w=majority'
 
 async function connectDb() {
 
     const client = new MongoClient(url,
-        { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    client
-        .connect()
-        .then((db) => {
-            mongoose = db;
-            return Promise.resolve();
-        })
-        .catch((err) => {
-            return Promise.reject(err);
-        });
+        {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
+
+    try{
+        db = await client.connect()
+        mongoose = db;
+
+        return Promise.resolve();
+    }
+    catch (err){
+        console.log(err)
+        return Promise.reject(err);
+    }
+
 }
 
 function getDb() {
